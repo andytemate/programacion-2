7282e6db838ae3f5cf79fe9d459e8a5746e129c7
Andy Santin <andycarlos.santin@udla.edu.ec>
2026-05-12 11:15:06 -0500

    primera vercion de impresion de arreglos con punteros

--- punteros.c
+++ punteros.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-int main ()
-{
-
+int main (){
 int arreglo[] = {2,4,6,8,10};
 int *ptr;
-ptr = &arreglo[0];
-printf("%p",ptr);
 
-for (int i= 0 ; i < 5 ; i++ ){
+ptr= &arreglo[0];
 
-    printf(" %d",arreglo[i]);
+for (int i =0; i <5 , i ++)
+{
+
+printf("arreglo [%d]= %d", i , arreglo [i]);
+ptr = &areglo [i];
+
+
 }
 
 }
