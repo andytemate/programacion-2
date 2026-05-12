8ded787e7fa6f4c85634d77b24635344361a7a18
Andy Santin <andycarlos.santin@udla.edu.ec>
2026-05-12 11:03:19 -0500

    se le aumento el puntero y ptr

--- punteros.c
+++ punteros.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-int main (){
+int main ()
+{
 
-int arreglo[] =(2,4,6,8,10);
-int tam=  5;
+int arreglo[] = {2,4,6,8,10};
+int *ptr;
+ptr = &arreglo[0];
+printf("%p",ptr);
 
-for (int i=0 ; i < tam; i++ ){
+for (int i= 0 ; i < 5 ; i++ ){
 
     printf(" %d",arreglo[i]);
 }
